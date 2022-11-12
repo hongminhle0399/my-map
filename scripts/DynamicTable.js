340161fcf23883ed60d5cf5ccd401c4db1bf4687
Emmy Tran <thuyuyenmytran@mail.fresnostate.edu>
2022-11-12 10:42:47 -0800

    update pages

--- scripts/DynamicTable.js
+++ scripts/DynamicTable.js
@@ -1,35 +1,35 @@
 class DynamicTable {
-  constructor(rowNum, colNum, tableClasses) {
-    this.rowNum = rowNum;
-    this.colNum = colNum;
-    this.tableClasses = tableClasses;
-    this.tableEl = undefined;
-
-    this._generate();
-  }
-
-  _createRow() {
-    let newRow = document.createElement('tr');
-    newRow.className = this.tableClasses.rowStyle;
-    return newRow;
-  }
-
-  _createColumn() {
-    let newCol = document.createElement('td');
-    newCol.className = this.tableClasses.colStyle;
-    return newCol;
-  }
-
-  _generate() {
-    this.tableEl = document.createElement('table');
-    this.tableEl.className = this.tableClasses.tableStyle;
-    for (let i = 0; i < this.rowNum; ++i) {
-      let row = this._createRow();
-      for (let j = 0; j < this.colNum; ++j) {
-        let col = this._createColumn();
-        row.appendChild(col);
+    constructor(rowNum, colNum, tableClasses) {
+      this.rowNum = rowNum;
+      this.colNum = colNum;
+      this.tableClasses = tableClasses;
+      this.tableEl = undefined;
+  
+      this._generate();
+    }
+  
+    _createRow() {
+      let newRow = document.createElement('tr');
+      newRow.className = this.tableClasses.rowStyle;
+      return newRow;
+    }
+  
+    _createColumn() {
+      let newCol = document.createElement('td');
+      newCol.className = this.tableClasses.colStyle;
+      return newCol;
+    }
+  
+    _generate() {
+      this.tableEl = document.createElement('table');
+      this.tableEl.className = this.tableClasses.tableStyle;
+      for (let i = 0; i < this.rowNum; ++i) {
+        let row = this._createRow();
+        for (let j = 0; j < this.colNum; ++j) {
+          let col = this._createColumn();
+          row.appendChild(col);
+        }
+        this.tableEl.appendChild(row);
       }
-      this.tableEl.appendChild(row);
     }
   }
-}
--- scripts/EventHandlers.js
+++ scripts/EventHandlers.js
@@ -1,25 +1,25 @@
 function onChangeOption(e) {
-  try {
-    let selectedOpt = e.target.value;
-    let [width, height] = OPTIONS[selectedOpt];
-    let table = new DynamicTable(width, height, {
-      tableStyle: 'game-table',
-      rowStyle: 'row',
-      colStyle: 'col',
-    });
-
-    let wrapperNode = document.querySelector('.wrapper');
-    let numOfChild = wrapperNode.children.length;
-    if (numOfChild > 0) {
-      for (let i = 0; i < numOfChild; ++i) {
-        if (wrapperNode.children[i].className === 'game-table') {
-          wrapperNode.children[i].remove();
-          break;
+    try {
+      let selectedOpt = e.target.value;
+      let [width, height] = OPTIONS[selectedOpt];
+      let table = new DynamicTable(width, height, {
+        tableStyle: 'game-table',
+        rowStyle: 'row',
+        colStyle: 'col',
+      });
+  
+      let wrapperNode = document.querySelector('.wrapper');
+      let numOfChild = wrapperNode.children.length;
+      if (numOfChild > 0) {
+        for (let i = 0; i < numOfChild; ++i) {
+          if (wrapperNode.children[i].className === 'game-table') {
+            wrapperNode.children[i].remove();
+            break;
+          }
         }
       }
+      wrapperNode.appendChild(table.tableEl);
+    } catch (error) {
+      console.log(`%c Error occured with on change option: ${error}`, 'background: red; padding: 0.2rem');
     }
-    wrapperNode.appendChild(table.tableEl);
-  } catch (error) {
-    console.log(`%c Error occured with on change option: ${error}`, 'background: red; padding: 0.2rem');
   }
-}
