db4fd228d5c7955e6227cd507d19b9bcc56df628
Emmy Tran <100542601+emmytran@users.noreply.github.com>
2022-11-13 21:19:41 -0800

    Create board-color.js

---
+++ scripts/board-color.js
@@ -0,0 +1,11 @@
+function toBlue() {
+    document.getElementById("wrapper").style.backgroundColor = "rgb(47, 58, 217)";
+}
+
+function toBlack() {
+    document.getElementById("wrapper").style.backgroundColor = "black";
+}
+
+function toWhite() {
+    document.getElementById("wrapper").style.backgroundColor = "#efeeee";
+}
